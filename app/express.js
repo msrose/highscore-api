74f59f5930ebecec4ab51e24aea5c426d3a71a77
Michael Rose <michael_rose@outlook.com>
2016-02-15 03:13:15 -0500

    Add cors

--- app/express.js
+++ app/express.js
@@ -4,15 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const config = require('../config/config');
 
 module.exports = () => {
   let app = express();
 
-  app.use(helmet());
   app.use(morgan(config.httpLogFormat));
 
+  app.use(helmet());
+  app.use(cors());
   app.use(bodyParser.json());
 
   require('./routes')(app);
